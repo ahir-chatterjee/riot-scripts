2ed9d90d8297a47eedc3057a4c1e6e2dd295c4c6
Ahir Chatterjee <ahir.chatterjee@TC-4000077s-MacBook-Pro.local>
2019-07-02 10:42:39 -0500

    added champ images

---
+++ dragontail-9.13.1/9.13.1/manifest.js
@@ -0,0 +1,1 @@
+Riot.DDragon.m={"n":{"item":"9.13.1","rune":"9.13.1","mastery":"9.13.1","summoner":"9.13.1","champion":"9.13.1","profileicon":"9.13.1","map":"9.13.1","language":"9.13.1","sticker":"9.13.1"},"v":"9.13.1","l":"en_US","cdn":null,"dd":"9.13.1","lg":"9.13.1","css":"9.13.1","profileiconmax":28};
